e8940438557b01832156a83d670a87972ab6438b
Your Username <your.email@example.com>
2023-08-30 14:28:49 -0400

    update

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ with open('requirements.txt') as f:
 setup(
     install_requires=requirements,
     include_package_data=True,
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.9',
