5d10cad19ad8b84f6a74a89a3eb0916114981e14
LzyFischer <vjd5zr@virginia.com>
2024-01-13 17:46:20 -0500

    unitttest

--- test/test.py
+++ test/test.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-class Test(unittest.TestCase):
 
-  def sum():
-    self.assertEqual(1,1)
+class Test(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+
+    def sum():
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
