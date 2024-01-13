58ce77c47ed36dfd98342d2fbc9ee3ab3ba44ebc
LzyFischer <vjd5zr@virginia.com>
2024-01-13 18:15:56 -0500

    unitttest

---
+++ test/test_o.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
