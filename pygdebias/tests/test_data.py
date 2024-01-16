ed2c9fdf5f3f19a09238d65f59f95e4a1d910563
LzyFischer <vjd5zr@virginia.com>
2024-01-16 15:02:12 -0500

    test file and gitignore update

--- pygdebias/tests/test_data.py
+++ pygdebias/tests/test_data.py
@@ -37,47 +37,44 @@ class Test(unittest.TestCase):
         self.assertEqual(1, 1)
 
     def test_initialize(self) -> None:
-        with assert_raises(ValueError):
-            pass
-            # self.google = Google()
-            # self.facebook = Facebook()
-            # self.oklahoma = Oklahoma()
-            # self.unc = UNC()
-            # self.twitter = Twitter()
-            # self.lastfm = Lastfm()
-            # self.nba = Nba()
-            # self.ml_1m = Ml_1m()
-            # self.ml_20m = Ml_20m()
-            # self.ml_100k = Ml_100k()
-            # self.german = German()
-            # self.bail = Bail()
-            # self.credit = Credit()
-            # self.cora = Cora()
-            # self.pokec_n = Pokec_n()
-            # self.pokec_z = Pokec_z()
-            # self.filmtrust = Filmtrust()
-            # self.citeseer = Citeseer()
-            # self.yelp = Yelp()
-            # self.amazon = Amazon()
-            # self.lcc = LCC()
-            # self.lcc_small = LCC_small()
-            # self.epinion = Epinion()
-            # self.ciao = Ciao()
-            # self.dblp = Dblp()
-            # self.german = German()
+        self.google = Google()
+        # self.facebook = Facebook()
+        # self.oklahoma = Oklahoma()
+        # self.unc = UNC()
+        # self.twitter = Twitter()
+        # self.lastfm = Lastfm()
+        # self.nba = Nba()
+        # self.ml_1m = Ml_1m()
+        # self.ml_20m = Ml_20m()
+        # self.ml_100k = Ml_100k()
+        # self.german = German()
+        # self.bail = Bail()
+        # self.credit = Credit()
+        # self.cora = Cora()
+        # self.pokec_n = Pokec_n()
+        # self.pokec_z = Pokec_z()
+        # self.filmtrust = Filmtrust()
+        # self.citeseer = Citeseer()
+        # self.yelp = Yelp()
+        # self.amazon = Amazon()
+        # self.lcc = LCC()
+        # self.lcc_small = LCC_small()
+        # self.epinion = Epinion()
+        # self.ciao = Ciao()
+        # self.dblp = Dblp()
+        # self.german = German()
 
     def test_api(self):
-        pass
-        # adj, features, idx_train, idx_val, idx_test, labels, sens, sens_idx = (
-        #     self.bail.adj(),
-        #     self.bail.features(),
-        #     self.bail.idx_train(),
-        #     self.bail.idx_val(),
-        #     self.bail.idx_test(),
-        #     self.bail.labels(),
-        #     self.bail.sens(),
-        #     self.bail.sens_idx(),
-        # )
+        adj, features, idx_train, idx_val, idx_test, labels, sens, sens_idx = (
+            self.bail.adj(),
+            self.bail.features(),
+            self.bail.idx_train(),
+            self.bail.idx_val(),
+            self.bail.idx_test(),
+            self.bail.labels(),
+            self.bail.sens(),
+            self.bail.sens_idx(),
+        )
 
 
 if __name__ == "__main__":
